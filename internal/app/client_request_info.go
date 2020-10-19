4d691711ff121083ecdfcbbf3f531afe16b77896
Denis Balabanov <balabanov.ds@gmail.com>
2020-10-19 06:53:08 +0300

    add name to parser Info

--- internal/app/client_request_info.go
+++ internal/app/client_request_info.go
@@ -4,12 +4,14 @@ import "github.com/balabanovds/smonitor/internal/models"
 
 type ParserInfo struct {
 	Type        models.ParserType
+	Name        string
 	MetricTypes []models.MetricType
 }
 
 func newParserInfo(ptype models.ParserType) ParserInfo {
 	return ParserInfo{
 		Type:        ptype,
+		Name:        ptype.String(),
 		MetricTypes: models.GetMetricTypes(ptype),
 	}
 }
